23e40013507ff1d9bebed08a4778c2ac8d72ee66
datrueonejay <jayarquelada@gmail.com>
2020-03-13 02:15:48 -0400

    Edits

--- websocket_server/app.js
+++ websocket_server/app.js
@@ -1,13 +1,29 @@
 const WebSocket = require("ws");
 let redis = require("redis");
-const client = redis.createClient(6379, "redis");
+const url = require("url");
+
+let http = require("http");
+
+const server = http.createServer();
+
+const client = redis.createClient(
+  process.env.REDIS_URL || "redis://localhost:6379"
+);
+// const client = redis.createClient(6379, "redis");
 // const client = redis.createClient();
 
-const wss = new WebSocket.Server({ port: 3001 }, () => {
-  console.log("SERVER SET UP");
-});
+// const wss = new WebSocket.Server({ server }, () => {
+//   console.log("SERVER SET UP");
+// });
+
+// const wss = new WebSocket.Server({ port: 8080 }, () => {
+//   console.log("SERVER SET UP");
+// });
+
+const wss = new WebSocket.Server({ noServer: true });
 
 wss.on("connection", function connection(ws) {
+  console.log("Trying to connect");
   ws.on("message", function incoming(message) {
     client.set("doc", message);
     wss.clients.forEach(client => {
@@ -24,3 +40,19 @@ wss.on("connection", function connection(ws) {
     }
   });
 });
+
+server.on("upgrade", (request, socket, head) => {
+  const pathname = url.parse(request.url).pathname;
+  console.log("pathname");
+  console.log(pathname);
+  if (pathname === "/") {
+    wss.handleUpgrade(request, socket, head, ws => {
+      console.log("Emitting request");
+      wss.emit("connection", ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(8080);
