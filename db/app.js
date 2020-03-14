37d0a36ae7c5f8dbea880ec8e85c7e3c30c72810
hackermanone <Ricky.chen98@hotmail.com>
2020-03-13 23:27:08 -0400

    cors policy

--- db/app.js
+++ db/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const query = require('./queries.js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("hello world");
