99c3e7df96b95a0578639e49f228024259bed131
Jacqueline Chan <jacquelinechan2@hotmail.com>
2020-03-14 23:20:02 -0400

    file upload and direct python compilation with ui and backend complete (jacqueline)

---
+++ client2/src/apiPython.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const serverurl = "http://localhost:3001";
+
+function sendFiles(method, url, data, callback){
+    url = serverurl + url;
+    let formdata = new FormData();
+    Object.keys(data).forEach(function(key){
+        let value = data[key];
+        formdata.append(key, value);
+    });
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status !== 200) callback("[" + xhr.status + "]" + xhr.responseText, null);
+        else callback(null, JSON.parse(xhr.responseText));
+    };
+    xhr.open(method, url, true);
+    xhr.send(formdata);
+}
+
+
+exports.compileText = function(code, callback=null){
+    sendFiles("POST", "/compile/plaintext/", {code: code}, callback);
+};
+
+exports.compileFile = function(file, callback=null){
+    sendFiles("POST", "/compile/file/", {file: file}, callback);
+};
