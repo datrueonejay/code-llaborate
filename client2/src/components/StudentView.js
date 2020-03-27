c76ab322d9e36cd40feb0beeacafa79da583274e
Jacqueline Chan <jacquelinechan2@hotmail.com>
2020-03-26 22:24:08 -0400

    added AppBar for StudentView - jacqueline

--- client2/src/components/StudentView.js
+++ client2/src/components/StudentView.js
@@ -3,9 +3,72 @@ import Editor from "../containers/Editor.js";
 import http from "../http";
 import Logout from "./Logout.js";
 
-function StudentView(props) {
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import {Button, Drawer,AppBar, CssBaseline, TextField, Toolbar, Typography, IconButton, Divider, List, ListItem} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+
+// credit: https://material-ui.com/components/drawers/
+
+const drawerWidth = 400;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginRight: drawerWidth,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    marginRight: 0,
+  },
+  contentShift: {
+    marginRight: drawerWidth,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+export default function StudentView(props) {
   const [courses, setCourses] = useState([]);
   const [isSession, setIsSession] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     http
@@ -19,8 +82,49 @@ function StudentView(props) {
   if (isSession) {
     return (
       <div>
-        <Logout />
-        <Editor isStudent={props.isStudent} />
+        <CssBaseline />
+        <AppBar position="sticky" className={clsx(classes.appBar, {[classes.appBarShift]: open,})}>
+          <Toolbar>
+            <Typography variant="h6" noWrap className={classes.title}>Persistent drawer</Typography>
+            <Logout />
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerOpen}
+              className={clsx(open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,}}
+        >
+          <div id="drawer_format">
+          <div>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+
+          <Divider />
+          
+          <TextField label="Write a message..."> </TextField>
+          <Button> Send </Button>
+
+          <Divider />
+          </div>
+        </Drawer>
+        <main className={clsx(classes.content, {[classes.contentShift]: open,})}>
+          <Editor isStudent={props.isStudent} />
+        </main>
+
       </div>
     );
   } else {
@@ -48,4 +152,4 @@ function StudentView(props) {
     )}
 }
 
-export default StudentView;
+//export default StudentView;
