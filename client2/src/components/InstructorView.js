80812802d00f6f8f4fc58aa777f067c4f2528665
datrueonejay <jayarquelada@gmail.com>
2020-03-28 19:19:58 -0400

    Merge

--- client2/src/components/InstructorView.js
+++ client2/src/components/InstructorView.js
@@ -1,7 +1,14 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from "react";
 import "../App.css";
 import styles from "../scss/InstructorView.module.scss";
-import { TextField, Button, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import {
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText
+} from "@material-ui/core";
 
 function InstructorView(props) {
   const api = require("../api.js");
@@ -12,7 +19,7 @@ function InstructorView(props) {
     studentId: "",
     courseId: "",
     studentIdFilter: "",
-    courseIdFilter: "",
+    courseIdFilter: ""
   });
 
   const formRef = useRef(null);
@@ -21,11 +28,15 @@ function InstructorView(props) {
 
   useEffect(() => {
     function updateStudents() {
-      api.getStudents((err, res) => {setStudents(res)});
+      api.getStudents((err, res) => {
+        setStudents(res);
+      });
     }
 
     function updateCourses() {
-      api.getCourses((err, res) => {setCourses(res)});
+      api.getCourses((err, res) => {
+        setCourses(res);
+      });
     }
 
     updateStudents();
@@ -46,7 +57,7 @@ function InstructorView(props) {
         setNotification("Successfully added student to the course");
       }
       formRef.current.reset();
-    })
+    });
   }
 
   // function setValue(ref, id) {
@@ -55,14 +66,14 @@ function InstructorView(props) {
   //     ref.current.focus();
   //   }
   // }
-  
+
   const setValue = (value, id) => event => {
-    setValues({...values, [value]: id});
-  }
+    setValues({ ...values, [value]: id });
+  };
 
   const handleChange = value => event => {
-    setValues({...values, [value]: event.target.value});
-  }
+    setValues({ ...values, [value]: event.target.value });
+  };
 
   function filterList(input, listRef) {
     return function(e) {
@@ -70,10 +81,10 @@ function InstructorView(props) {
       input = input.toUpperCase(); // what the user typed in
       let list = listRef.current.querySelectorAll("li");
 
-      for(let i = 0; i < list.length ; i++) {
+      for (let i = 0; i < list.length; i++) {
         let li, text;
         li = list[i];
-        text = li.textContent|| li.innerText; // text of the list
+        text = li.textContent || li.innerText; // text of the list
         text = text.toUpperCase();
         if (text.indexOf(input) > -1) {
           li.classList.remove("hide");
@@ -81,44 +92,44 @@ function InstructorView(props) {
           li.classList.add("hide");
         }
       }
-      
-    }
+    };
   }
 
   // TODO: Clean up this pile of mess
   return (
     <div>
       <form className={styles.form} ref={formRef} onSubmit={handleSubmit}>
-
         <h1 className="text-center">Add student to course</h1>
 
-        <div className={styles['form-input']}>
-          <TextField 
+        <div className={styles["form-input"]}>
+          <TextField
             id="studentID"
             fullWidth={true}
             label="Student Id"
             name="studentID"
-            onChange={handleChange('studentId')}
+            onChange={handleChange("studentId")}
             value={values.studentId}
             helperText="The student id, for example, 3"
             required
           />
         </div>
 
-        <div className={styles['form-input']}>
-          <TextField 
+        <div className={styles["form-input"]}>
+          <TextField
             id="courseID"
             fullWidth={true}
             label="Course Id"
             name="courseID"
-            onChange={handleChange('courseId')}
+            onChange={handleChange("courseId")}
             value={values.courseId}
             helperText="The course id, for example, 2"
             required
           />
         </div>
-        
-        <Button type="submit" color="primary" variant="contained">Add student to course</Button>
+
+        <Button type="submit" color="primary" variant="contained">
+          Add student to course
+        </Button>
 
         <div className="notification">{notification}</div>
       </form>
@@ -127,54 +138,59 @@ function InstructorView(props) {
         <h1>Students:</h1>
 
         <div id="filter-student">
-          <TextField 
-            className={styles.input} 
-            type="text" 
-            onKeyUp={filterList(values.studentIdFilter, studentListRef)} 
-            onChange={handleChange('studentIdFilter')} 
-            value={values.studentIdFilter} />
+          <TextField
+            className={styles.input}
+            type="text"
+            onKeyUp={filterList(values.studentIdFilter, studentListRef)}
+            onChange={handleChange("studentIdFilter")}
+            value={values.studentIdFilter}
+          />
         </div>
 
-        <ul className={styles['list-wrapper']} ref={studentListRef}>
-          {students.map((student) => {
-            return <li 
-                    className={styles.list} 
-                    onClick={setValue("studentId",student.ID)} 
-                    key={student.ID} 
-                    id={student.ID}
-                    >
-                    {student.Name} id: {student.ID}
-                    </li>
+        <ul className={styles["list-wrapper"]} ref={studentListRef}>
+          {students.map(student => {
+            return (
+              <li
+                className={styles.list}
+                onClick={setValue("studentId", student.ID)}
+                key={student.ID}
+                id={student.ID}
+              >
+                {student.Name} id: {student.ID}
+              </li>
+            );
           })}
         </ul>
 
         <h1>Courses:</h1>
 
         <div id="filter-course">
-          <TextField 
-            className={styles.input} 
-            type="text" 
-            onKeyUp={filterList(values.courseIdFilter, courseListRef)} 
-            onChange={handleChange('courseIdFilter')} 
-            value={values.courseIdFilter} />
+          <TextField
+            className={styles.input}
+            type="text"
+            onKeyUp={filterList(values.courseIdFilter, courseListRef)}
+            onChange={handleChange("courseIdFilter")}
+            value={values.courseIdFilter}
+          />
         </div>
-        
-        <ul ref={courseListRef} className={styles['list-wrapper']}>
-        {courses.map((course, index) => {
-            return <li 
-              className={styles.list} 
-              onClick={setValue("courseId", course.ID)} 
-              key={index} 
-              id={course.ID}
+
+        <ul ref={courseListRef} className={styles["list-wrapper"]}>
+          {courses.map((course, index) => {
+            return (
+              <li
+                className={styles.list}
+                onClick={setValue("courseId", course.ID)}
+                key={index}
+                id={course.ID}
               >
-              {course.CourseCode}
+                {course.CourseCode}
               </li>
+            );
           })}
         </ul>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default InstructorView;
