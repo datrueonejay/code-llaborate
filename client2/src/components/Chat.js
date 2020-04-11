582f4bf8f570f2f690db77cc1d6e284821179bcc
Jayden Arquelada <jayarquelada@gmail.com>
2020-04-11 05:29:42 -0400

    Chat Styling

--- client2/src/components/Chat.js
+++ client2/src/components/Chat.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import http from "../http";
 import { Form } from "react-bootstrap";
-import  "../scss/Chat.scss"
+import "../scss/Chat.scss";
+import { FixedSizeList as List } from "react-window";
 
 import {
   Button,
@@ -13,42 +14,75 @@ import {
   Typography,
   IconButton,
   Divider,
-  List,
   ListItem,
   CircularProgress,
+  Card,
+  CardContent,
 } from "@material-ui/core";
+import useSharedStyles from "../styles/SharedStyles.module";
+import useStyles from "../styles/ChatStyles.module";
 
 function Chat(props) {
   let handleSubmitChat = (event) => {
     event.preventDefault();
     props.sendChat(document.querySelector("#chatText").value);
-    // let chat = document.querySelector("#chatText").value;
-    // http.send_message(chat, "CHAT");
   };
 
+  // TODO: Fill in username/possibly time?
+
+  let Chat = ({ index, style }) => {
+    return (
+      <div style={style}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography color="textPrimary">Anonymous</Typography>
+            <Typography color="textSecondary">
+              {props.chatOut[index]}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  };
+
+  const sharedStyles = useSharedStyles();
+  const styles = useStyles();
   return (
     <div>
-      <h1>Chat</h1>
-      <nav className="listMessage">
-
-      <ul>
-        {props.chatOut.map((chat, index) => {
-          return (
-            <li key={index} id={chat}>
-              {chat}
-            </li>
-          );
-        })}
-      </ul>
-
-      </nav>
+      <Typography align="center" variant="h4" color="textPrimary">
+        Chat
+      </Typography>
+      {/* <div className={sharedStyles.subTitle}>Chat</div> */}
+      <List
+        height={props.height || 400}
+        itemSize={150}
+        itemCount={props.chatOut.length}
+        width="100%"
+      >
+        {Chat}
+      </List>
+      {/* <nav className="listMessage">
+        <ul>
+          {props.chatOut.map((chat, index) => {
+            return (
+              <li key={index} id={chat}>
+                {chat}
+              </li>
+            );
+          })}
+        </ul>
+      </nav> */}
       <Form onSubmit={handleSubmitChat}>
         <div>
-        <TextField label="Send a message..." name="chat" className="chatText" id="chatText" > </TextField>
-        <Button type="submit" className="btn">
-          {" "}
-          Chat{" "}
-        </Button>
+          <TextField
+            label="Send a message..."
+            name="chat"
+            className="chatText"
+            id="chatText"
+          ></TextField>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Send Message
+          </Button>
         </div>
       </Form>
     </div>
--- client2/src/components/Drawer.js
+++ client2/src/components/Drawer.js
@@ -6,22 +6,22 @@ import websocket from "../http/socketController.js";
 
 import clsx from "clsx";
 import {
-    Button,
-    Drawer,
-    AppBar,
-    CssBaseline,
-    TextField,
-    Toolbar,
-    Typography,
-    IconButton,
-    Divider,
-    List,
-    ListItem,
-    CircularProgress,
-  } from "@material-ui/core";
+  Button,
+  Drawer,
+  AppBar,
+  CssBaseline,
+  TextField,
+  Toolbar,
+  Typography,
+  IconButton,
+  Divider,
+  List,
+  ListItem,
+  CircularProgress,
+} from "@material-ui/core";
 
-import {useStyles} from "../styles/StudentPageStyle.js";
-import {useTheme} from "@material-ui/core/styles";
+import { useStyles } from "../styles/StudentPageStyle.js";
+import { useTheme } from "@material-ui/core/styles";
 
 import Logout from "../components/Logout.js";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -29,80 +29,79 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 let handleSubmitChat = (event) => {
-    event.preventDefault();
-    let chat = document.querySelector("#chatText").value;
-    // http.send_message(chat, "CHAT");
-  };
+  event.preventDefault();
+  let chat = document.querySelector("#chatText").value;
+  // http.send_message(chat, "CHAT");
+};
 
 export default function StudentView(props) {
+  const [open, setOpen] = useState(false);
+  const [chatOut, setChatOut] = useState([]);
 
-    const [open, setOpen] = useState(false);
-    const [chatOut, setChatOut] = useState([]);
+  const classes = useStyles();
+  const theme = useTheme();
 
-    const classes = useStyles();
-    const theme = useTheme();
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-      };
-    
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    return (
+  return (
     <div>
-        <AppBar
+      <AppBar
         position="sticky"
         className={clsx(classes.appBar, { [classes.appBarShift]: open })}
-        >
+      >
         <Toolbar>
-        <Typography variant="h6" noWrap className={classes.title}>
+          <Typography variant="h6" noWrap className={classes.title}>
             Code-llaborate
-        </Typography>
-        <Logout />
-        <IconButton
+          </Typography>
+          <Logout />
+          <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="end"
             onClick={handleDrawerOpen}
             className={clsx(open && classes.hide)}
-        >
+          >
             <MenuIcon />
-        </IconButton>
+          </IconButton>
         </Toolbar>
-        </AppBar>
-        <Drawer
+      </AppBar>
+      <Drawer
         className={classes.drawer}
         variant="persistent"
         anchor="right"
         open={open}
         classes={{
-        paper: classes.drawerPaper,
+          paper: classes.drawerPaper,
         }}
-        >
+      >
         <div id="drawer_format">
-        <div>
+          <div>
             <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
+              {theme.direction === "rtl" ? (
                 <ChevronRightIcon />
-            ) : (
+              ) : (
                 <ChevronLeftIcon />
-            )}
+              )}
             </IconButton>
-        </div>
+          </div>
 
-        <Chat
-          chatOut={props.chatOut}
-          sendChat={(message) => {
-            console.log(message);
-            websocket.send_chat(message);
-          }}
-        ></Chat>
+          <Chat
+            chatOut={props.chatOut}
+            sendChat={(message) => {
+              console.log(message);
+              websocket.send_chat(message);
+            }}
+          ></Chat>
 
-        <Divider />
+          <Divider />
         </div>
-        </Drawer>
+      </Drawer>
     </div>
-    )}
+  );
+}
