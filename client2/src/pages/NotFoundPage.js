63a8a661c8d8138172555c2540d35779522f9891
Ricky Chen <Ricky.chen98@hotmail.com>
2020-04-12 15:22:57 -0400

    404 pages

--- client2/src/pages/NotFoundPage.js
+++ client2/src/pages/NotFoundPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import useSharedStyles from "../styles/SharedStyles.module";
+import { Typography } from '@material-ui/core';
 
 export default function NotFoundPage() {
+  const styles = useSharedStyles();
 
   return (
-    <div>
-      Could not find the page you're looking for? 
-      Contact `jayjayrayc09@gmail.com` to see who you deduct marks from!
+    <div className={styles.background} style={{height: "100%"}}>
+      <Typography color="textPrimary" variant="h1">
+        Could not find the page you're looking for? 
+        Contact <span style={{color: "blue"}}>{"<jayjayrayc09@gmail.com>"}</span> to see who you deduct marks from!
+      </Typography>
     </div>
   );
 }
